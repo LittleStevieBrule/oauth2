ba597c0919510f10d9c14d67a3061bd3d5e0b5cb
StephenMcGuckinDAT <stephendmcguckin@gmail.com>
2017-10-22 21:03:51 -0700

    final content route and helper methods

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -20,6 +20,8 @@ module OauthHelpers
     '&redirect_uri='\
       "#{config.redirect_uri}"\
     '&response_type=code&'\
+    '&state='\
+      "#{config.state}"\
     '&client_id='\
       "#{config.client_id}"
   end
@@ -33,18 +35,24 @@ module OauthHelpers
       redirect_uri: config.redirect_uri,
       grant_type: 'authorization_code'
     }
-    result = post_form(uri, data)
-    @token = Json.parse(result.body)
+    Net::HTTP.post_form(uri, data).body
+  end
+
+  def get_my_info(token)
+    token = JSON.parse(token)
+    uri = URI(
+      'https://www.googleapis.com/plus/v1/people/me?'\
+      "access_token=#{token['access_token']}"
+    )
+    JSON.parse(Net::HTTP.get(uri))
   end
 
   def _render(template, options = {})
     options.each { |k, v| instance_variable_set("@#{k}", v) }
-    template = File.read(template)
+    template = File. read(template)
     ERB.new(template).result(binding)
   end
 
-  def post_form(uri, params)
-    Net::HTTP.post_form(uri, params)
-  end
+  #GET https://www.googleapis.com/plus/v1/people/
 
 end
